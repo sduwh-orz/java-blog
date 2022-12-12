b5f60cfad89ad2c02f3e7b2096fd7ee574b857d1
xszqxszq <zhangqianren2003@sina.com>
2022-12-12 21:13:24 +0800

    CI workflow

--- src/main/java/cn/edu/sdu/orz/JavaBlogApplication.java
+++ src/main/java/cn/edu/sdu/orz/JavaBlogApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaBlogApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test2");
 		SpringApplication.run(JavaBlogApplication.class, args);
 	}
 
