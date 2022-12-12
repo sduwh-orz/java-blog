211114614f00991c2f3546418fac110632ecc2bc
xszqxszq <zhangqianren2003@sina.com>
2022-12-12 21:47:45 +0800

    Restore code

--- src/main/java/cn/edu/sdu/orz/JavaBlogApplication.java
+++ src/main/java/cn/edu/sdu/orz/JavaBlogApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaBlogApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test3");
 		SpringApplication.run(JavaBlogApplication.class, args);
 	}
 
