9a791f5430a18e0fdade2532e90147189f06c0ba
xszqxszq <zhangqianren2003@sina.com>
2022-12-12 21:22:26 +0800

    test3

--- src/main/java/cn/edu/sdu/orz/JavaBlogApplication.java
+++ src/main/java/cn/edu/sdu/orz/JavaBlogApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaBlogApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		System.out.println("test3");
 		SpringApplication.run(JavaBlogApplication.class, args);
 	}
 
